152a366604cf26676a171664ae28d802a5fd1107
Tom Peter <tompeter18@gmail.com>
2024-06-05 14:12:10 +0530

    final nse

---
+++ nse.js
@@ -0,0 +1,11 @@
+#betaVerBand {
+    font-size: 12px;
+    background: #392d7d;
+    color: #fff;
+    width: 100%;
+    text-align: center;
+    padding: 4px;
+    position: fixed;
+    top: 0px;
+    z-index: 9999;
+}
